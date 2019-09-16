75df6361c09bd81ef564a531f0e0fc8ca569d325
baggio_czm <baggio_tnt@163.com>
2019-09-16 14:12:21 +0800

    webview 加载效果

--- lib/pages/NewsDetail.dart
+++ lib/pages/NewsDetail.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_inappbrowser/flutter_inappbrowser.dart';
 
+// ignore: must_be_immutable
 class NewsDetail extends StatefulWidget {
   Map arguments;
   NewsDetail({Key key, this.arguments}): super(key: key);
@@ -11,7 +12,10 @@ class NewsDetail extends StatefulWidget {
 class _NewsDetailState extends State<NewsDetail> {
   Map arguments;
   List detail=[];
+  bool _flag= true;
+
   _NewsDetailState(this.arguments);
+
   @override
   void initState() {
     // TODO: implement initState
@@ -24,12 +28,44 @@ class _NewsDetailState extends State<NewsDetail> {
       appBar: AppBar(title:Text('新闻详情-flutter_inappbrowser调用webview')),
       body: Column(
         children: <Widget>[
+          this._flag?_getMoreWidget():Text(""),
           Expanded(child:
           InAppWebView(
             initialUrl: "http://www.phonegap100.com/newscontent.php?aid=${this.arguments["aid"]}",
 //            initialUrl: "http://www.guilinenguang.org/public/love/#/sermons/${arguments['aid']}/sermons",
+              onProgressChanged: (InAppWebViewController controller, int progress) {
+                print(progress/100);
+                if((progress/100)>0.999){
+                  setState(() {
+                    this._flag=false;
+                  });
+                }
+              }
           ))
         ],
+      ),
+    );
+  }
+
+
+  //加载中的圈圈
+  Widget _getMoreWidget() {
+    return Center(
+      child: Padding(
+        padding: EdgeInsets.all(10.0),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              '加载中...',
+              style: TextStyle(fontSize: 16.0),
+            ),
+            CircularProgressIndicator(
+              strokeWidth: 1.0,
+            )
+          ],
+        ),
       ),
     );
   }
